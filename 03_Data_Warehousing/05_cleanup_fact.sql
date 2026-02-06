84ffaff87448a826f2732cee4d4062e6ada7c289
Kamran Namazov <knamazov28@gmail.com>
2026-02-06 16:18:48 +0400

    Update 05_cleanup_fact.sql

--- 03_Data_Warehousing/05_cleanup_fact.sql
+++ 03_Data_Warehousing/05_cleanup_fact.sql
@@ -8,10 +8,9 @@ ALTER TABLE Fact_Complaints DROP COLUMN Temp_Issue;
 ALTER TABLE Fact_Complaints DROP COLUMN Temp_Sub_Issue;
 
 -- 1. Drop the Constraint (Key) first
--- (I copied this specific ID from your error message)
 ALTER TABLE Fact_Complaints DROP CONSTRAINT PK_Fact_Complaints;
 
--- 2. NOW you can drop the column
+-- 2. 
 ALTER TABLE Fact_Complaints DROP COLUMN Row_ID;
 
 -- 3. Finish the cleanup
